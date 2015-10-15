b21864f2cfceb5fd18c2d4a99dad194c0eff3c69
steve <nowman120@gmail.com>
2015-10-14 19:11:35 -0600

    Made sure to not add duplicate data to database for a stock

--- getStockPrices/handleMongoDB.py
+++ getStockPrices/handleMongoDB.py
@@ -2,20 +2,35 @@
 from pymongo import MongoClient
 
 
+# insert a row of data from the stock
 def insert_data(stock, open, high, low, close, volume, adjClose, date):
     client = MongoClient()
-    db = client.stockDB  # switches the client to point at 'stockDB' database
+    db = client.stockDB
 
-    result = db.restaurants.insert_one(
-        {
-            "stockIndex": stock,
-            "date": date,
-            "open": open,
-            "high": high,
-            "low": low,
-            "close": close,
-            "volume": volume,
-            "adjClose": adjClose
-        }
-    )
+    result = db.findOne({"stockIndex": stock, "date": date})
+
+    # only insert data if we don't already have info for this date
+    if result is not None:
+
+        db.stockDB.insert_one(
+            {
+                "stockIndex": stock,
+                "date": date,
+                "open": open,
+                "high": high,
+                "low": low,
+                "close": close,
+                "volume": volume,
+                "adjClose": adjClose
+            }
+        )
+    return
+
+
+# clean out the database
+def clear_db():
+    client = MongoClient()
+    db = client.stockDB
+
+    db.dropDatabase()
     return
--- getStockPrices/parseYahoo.py
+++ getStockPrices/parseYahoo.py
@@ -8,11 +8,13 @@ import handleMongoDB
 
 stockFile = open('stockIndexes.txt', 'r')
 
+# loop over every stock index in the file and store in the database
 for stock in stockFile:
 
     page = requests.get('http://finance.yahoo.com/q/hp?s=%s+Historical+Prices' % stock)
     tree = html.fromstring(page.text)
 
+    # grab the data out of the yahoo finance table
     headers = tree.xpath('//th[@class="yfnc_tablehead1"]/text()')
     dataArray = tree.xpath('//td[@class="yfnc_tabledata1"]/text()')
 
@@ -24,6 +26,7 @@ for stock in stockFile:
 
     print '%s' % stock
 
+    # process the data in order
     counter = 0
     for data in range(0, num_of_loops):
         infoDate = dataArray[counter]
@@ -38,5 +41,6 @@ for stock in stockFile:
         # that way we don't need to worry about formatting any more
         dateObject = datetime.datetime.strptime(infoDate, "%b %d, %Y")
 
+        # store the data in the database
         handleMongoDB.insert_data(stock, infoOpen, infoHigh, infoLow,
                                   infoClose, infoVolume, infoAdjClose, dateObject)
